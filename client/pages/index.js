c8798e40240d7edeac1b9251e77eee7537a443ff
Jerome Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2021-02-15 15:08:05 -0500

    server side rendering with Next.js

---
+++ client/pages/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    //we are on the server so requests should be made to http://ingress-nginx-...
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    );
+    return data;
+
+  } else {
+    //we are on the browser so request can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
